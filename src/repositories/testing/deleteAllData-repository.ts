61e7d1cb105b45f23203ec78c9e0ab5c49b8cca5
snitovlad <snitovov@gmail.com>
2024-06-09 20:08:47 +0300

    Chenged deleteAllData() with Promise.all()

--- src/repositories/testing/deleteAllData-repository.ts
+++ src/repositories/testing/deleteAllData-repository.ts
@@ -1,14 +1,30 @@
 import { blogCollection, commentCollection, postCollection, userCollection } from "../../db/mongo-db";
 
+// export const deleteAllDataRepository = {
+//     async deleteAllData(): Promise<{ success?: boolean }> {
+
+//         try {
+//             await postCollection.deleteMany({})
+//             await blogCollection.deleteMany({})
+
+//             await userCollection.deleteMany({})
+//             await commentCollection.deleteMany({})
+//             return { success: true }
+//         } catch (e) {
+//             throw new Error('Failed to delete all data')
+//         }
+//     }
+// }
+
 export const deleteAllDataRepository = {
     async deleteAllData(): Promise<{ success?: boolean }> {
-
         try {
-            await postCollection.deleteMany({})
-            await blogCollection.deleteMany({})
-
-            await userCollection.deleteMany({})
-            await commentCollection.deleteMany({})
+            await Promise.all([
+                postCollection.deleteMany({}),
+                blogCollection.deleteMany({}),
+                userCollection.deleteMany({}),
+                commentCollection.deleteMany({})
+            ]);
             return { success: true }
         } catch (e) {
             throw new Error('Failed to delete all data')
